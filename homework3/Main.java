6ff29ec927726143ef32a51de2b7f67d1b042a61
mkochman22 <95151862+mkochman22@users.noreply.github.com>
2022-02-12 16:47:25 -0500

    Update Main.java

--- homework3/Main.java
+++ homework3/Main.java
@@ -1,7 +1,10 @@
+//Mary Kochmanski, CIS 221 Tuesdays and Thursdays at 2:10
 public class Main {
   public static void main(String[] args) {
     Agent myObj = new Agent();
+    //set name variable
     myObj.setName("Killjoy");
+    //print name
     System.out.println(myObj.getName());
   }
 }
